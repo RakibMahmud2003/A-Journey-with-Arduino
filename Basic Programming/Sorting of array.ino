95f50fe7e5b8aee344933b1ee9de0313d9ccab7f
Rakib Mahmud <139487805+RakibMahmud2003@users.noreply.github.com>
2023-07-16 23:42:05 +0600

    Create Sorting of array.ino

---
+++ Basic Programming/Sorting of array.ino
@@ -0,0 +1,35 @@
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  int array[5] = {4,3,2,5,1};
+  int temp,i,j;
+  
+  Serial.print("Before Sorting : ");
+  
+  for( i = 0; i<5 ; i++){
+   Serial.print(array[i]); 
+  }
+  
+  
+  for(i = 0;i<5;i++){
+    for(j = i+1; j<6 ; j++){
+      if(array[i]>array[j]){
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    }
+  }
+  
+  Serial.println("After Sorting: ");
+  for(i=0 ; i<5 ; i++){
+   Serial.print(array[i]); 
+  }
+  
+}
+void loop()
+{
+  
+}
